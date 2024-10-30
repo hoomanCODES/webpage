ba291b0b00f3caf51458e2236343e508d33a8912
Hoomanwise <bhavyasharma09283@gmail.com>
2024-10-30 20:10:11 +0530

    added shit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# webpage
-my webpage
+# this is my website!
+## cool website ik frfr
+
+ And yes, i know. I suck at frontend
+
+ ![logo](https://github.com/JayyDoesDev/personal-site/blob/main/.github/assets/site.png?raw=true)
---
+++ js/presence.js
@@ -0,0 +1,32 @@
+async function getPresence() {
+  try {
+    var fetched = await fetch(
+      "https://japi.rest/discord/v1/user/419958345487745035"
+    );
+    var res = await fetched.json();
+    var status = res.presence.status;
+    var statusIcon = document.getElementById("status");
+    console.log(statusIcon.src)
+    console.log(status)
+    switch (status) {
+      case "online":
+        statusIcon.src = "/img/statuses/online.png";
+        break;
+      case "idle":
+        statusIcon.src = "/img/statuses/idle.png";
+        break;
+      case "dnd":
+        statusIcon.src = "/img/statuses/dnd.png";
+        break;
+      case "streaming":
+        statusIcon.src = "/img/statuses/streaming.png";
+        break;
+      default:
+        statusIcon.src = "/img/statuses/offline.png";
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getPresence();
